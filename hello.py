53804f5e1b8a0a31c66d4ff3c1e0224ec7c0f414
leibnitz25 <leibnitz25@gmail.com>
2020-09-07 21:20:02 +0300

    called outside function 2

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+import outside_functions
+
 def double_it(n):
     return(2 * n)
 
 def main():
     print("Main function called")
     print(double_it(25))
+    outside_functions.outside2()
 
 main()
---
+++ outside_functions.py
@@ -0,0 +1,5 @@
+def outside1():
+    print("succesfully called outside function 1")
+
+def outside2():
+    print("succesfully called outside function 2")
