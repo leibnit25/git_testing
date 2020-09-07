8ddec14d11e18da072bf2afe426ec6e8c3cf555e
leibnitz25 <leibnitz25@gmail.com>
2020-09-07 21:02:25 +0300

    removed trash from hello

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
-print("Hello world!")
-
-for i in range(0, 20):
-    print(i)
-
 def double_it(n):
     return(2 * n)
 
 def main():
+    print("Main function called")
     print(double_it(25))
 
 main()
